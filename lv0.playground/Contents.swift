481671389f112fb751ebd7d1f3f85e1304c5fdb2
zzee22su <patrickstar0910@gmail.com>
2022-12-28 15:42:06 +0900

    ADD Level 0

---
+++ lv0.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+func solution(_ n:Int) -> Int {
+    var sum: Int = 0;
+    
+    for i in 1...n  {
+     if(i%2 == 0) {
+        sum += i
+        }
+    }
+    return sum
+}
+
+solution(10)
