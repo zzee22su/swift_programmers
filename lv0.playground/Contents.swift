d2caa949dc8c711ddb9b2e1e34daf9181374e044
zzee22su <patrickstar0910@gmail.com>
2022-12-30 20:40:16 +0900

    Add average value of array

--- lv0.playground/Contents.swift
+++ lv0.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+//Lv.0 - 짝수의 합
 func solution(_ n:Int) -> Int {
     var sum: Int = 0;
     
@@ -12,3 +13,18 @@ func solution(_ n:Int) -> Int {
 }
 
 solution(10)
+
+//Lv.0 - 배열의 평균 값
+func average(_ numbers:[Int]) -> Double {
+    var sum: Int = 0;
+    for i in 0...numbers.count-1 {
+        sum += numbers[i]
+    }
+    return Double(sum)/Double(numbers.count)
+}
+
+var num: [Int] = [1,2,3]
+
+average(num)
+
+
